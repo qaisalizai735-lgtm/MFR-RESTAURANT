a00cf0d9eb3de34aa9db4d11f8917a435298f541
qaisalizai735-lgtm <qaisalizai735@gmail.com>
2026-04-08 20:59:26 +0500

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,8 +1,36 @@
 export default function App() {
   return (
-    <div>
-      <h1>MFR Restaurant 🍔</h1>
-      <p>Website is working successfully 🚀</p>
+    <div style={{ fontFamily: "Arial", padding: "20px" }}>
+      
+      <h1 style={{ color: "#ff4d4d" }}>🍔 MFR Restaurant</h1>
+      <p>Delicious Fast Food | Juices | Ice Cream</p>
+
+      <hr />
+
+      <h2>🔥 Our Menu</h2>
+
+      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
+
+        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+          <h3>Burger</h3>
+          <p>Rs 500</p>
+          <button>Order</button>
+        </div>
+
+        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+          <h3>Pizza</h3>
+          <p>Rs 1200</p>
+          <button>Order</button>
+        </div>
+
+        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+          <h3>Juice</h3>
+          <p>Rs 250</p>
+          <button>Order</button>
+        </div>
+
+      </div>
+
     </div>
   );
 }

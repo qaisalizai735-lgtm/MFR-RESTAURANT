f61ec1a7727f1c542b3d5ee7a725edffa9374bd9
qaisalizai735-lgtm <qaisalizai735@gmail.com>
2026-04-08 21:05:16 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 style={{color:"blue"}}>DIRECT WORKING ✅</h1>
+  <h1 style={{ color: "blue" }}>DIRECT WORKING ✅</h1>
 );

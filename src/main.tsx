6340c00d20ec21e7025a8144b32aab1ccee9df7f
qaisalizai735-lgtm <qaisalizai735@gmail.com>
2026-04-08 21:03:17 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-  return <h1>MFR Restaurant Live 🚀</h1>;
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <h1 style={{color:"blue"}}>DIRECT WORKING ✅</h1>
 );
